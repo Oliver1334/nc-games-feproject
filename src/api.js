06b23a420071cddc05e3ac7d555250b04b355b00
Oliver1334 <oliver.taylor95@outlook.com>
2024-06-08 22:36:11 +0100

    comment posting error work

--- src/api.js
+++ src/api.js
@@ -1,44 +1,55 @@
-import axios from 'axios';
-
+import axios from "axios";
 const ncGamesApi = axios.create({
-    baseURL: "https://nc-games-2kfx.onrender.com/api/"
+  baseURL: "https://nc-games-2kfx.onrender.com/api",
 });
 
-export const fetchReviews = (category = null) =>{
+export const fetchReviews = (category = null) => {
+  const filterURL = category ? `?category=${category}` : "";
 
-const filterURL = (category) ? `?category=${category}` : '';
-
-    return ncGamesApi.get(`/reviews${filterURL}`).then((results) => { 
-        return results.data.reviews; 
-    })
+  return ncGamesApi.get(`/reviews${filterURL}`).then((results) => {
+    return results.data.reviews;
+  });
 };
 
 export const fetchCats = () => {
-    return ncGamesApi.get("/categories")
-    .then((results) => {
-        return results.data.categories;
-    })
-}
+  return ncGamesApi.get("/categories").then((results) => {
+    return results.data.categories;
+  });
+};
 
 export const getReviewById = (id) => {
-
-    return ncGamesApi.get(`/reviews/${id}`)
-    .then((result) => {
-        return result.data.review
-    })
-}
+  return ncGamesApi.get(`/reviews/${id}`).then((result) => {
+    return result.data.review;
+  });
+};
 
 export const getCommentsById = (id) => {
+  return ncGamesApi.get(`/reviews/${id}/comments`).then(({ data }) => {
+    return data.comments;
+  });
+};
 
-    return ncGamesApi.get(`/reviews/${id}/comments`)
-    .then(({data}) => {
-        return data.comments
+export const postCommentHandler = ({ review_id, user, inputComment }) => {
+  console.log(user)
+  console.log(review_id)
+  console.log(inputComment)
+  return ncGamesApi.post(`/reviews/${review_id}/comments`, {
+      username: user,
+      body: inputComment,
     })
-}
+    .then((res) => {
+      console.log(res);
+      return res.data[0];
+    })
+    .catch((err) => {
+      console.log(err, "<err");
+    });
+};
 
 export const voteForReview = (review_id) => {
-    return ncGamesApi.patch(`/reviews/${review_id}`, {inc_votes: 1})
-    .then(({data}) => {
-        return data.review;
+  return ncGamesApi
+    .patch(`/reviews/${review_id}`, { inc_votes: 1 })
+    .then(({ data }) => {
+      return data.review;
     });
-}
+};
--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -1,38 +1,90 @@
-
-import { getCommentsById } from '../api';
-import { useState, useEffect } from 'react';
-import CommentCard from './CommentCard';
+import { getCommentsById, postCommentHandler } from "../api";
+import { useState, useEffect } from "react";
+import CommentCard from "./CommentCard";
+import { UserContext } from "../contexts/UserContext"
 
 const CommentList = (passed_id) => {
-    const {review_id} = passed_id
-    console.log(review_id)
+  const { review_id } = passed_id;
+  const [comments, setComments] = useState([]);
+  const [inputComment, setInputComment] = useState("");
+  const [commentError, setCommentError] = useState(false);
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const user = "jessjelly"
 
+  console.log(comments);
 
-const [comments, setComments] = useState([])
-
-console.log(comments)
-
-useEffect(() => {
+  useEffect(() => {
     getCommentsById(review_id).then((results) => {
-        setComments(results);
-    })
-}, [])
+      setComments(results);
+    });
+  }, [review_id]);
 
+  const typeComment = (event) => {
+    setCommentError(false);
+    setInputComment(event.target.value);
+    console.log(inputComment);
+  };
 
-if (!comments) return <p>Loading...</p>
+  const submitComment = (event) => {
+    if (inputComment.length < 2) {
+      setCommentError(true);
+    } else {
+      setButtonDisabled(true);
+      postCommentHandler({ review_id, user, inputComment }).then(
+        (returnedComment) => {
+          setButtonDisabled(false);
+          setInputComment("");
+          setComments((currentComments) => {
+            return [returnedComment, ...currentComments];
+          });
+        }
+      );
+    }
+  };
 
-return (
-    <section id="comment-list">
+  if (!comments) return <p>Loading...</p>;
+
+  return (
+    <div className="comments">
+      <h3>Comments</h3>
+      <form id="post-comment">
+        <label htmlFor="comment-box">Join the Conversation!</label>
+        <br />
+        <textarea
+          id="comment-box"
+          onChange={typeComment}
+          value={inputComment}
+        ></textarea>
+        <br />
+
+        <button
+          id="submit-comment"
+          onClick={(event) => {
+            event.preventDefault();
+            submitComment();
+          }}
+          disabled={buttonDisabled}
+        >
+          {buttonDisabled ? "Please Wait..." : "Post a Comment"}
+        </button>
+          <span id="comment-length-error" className="error-box">
+            {commentError ? (
+                <p>
+                    Comments must contain at least 2 characters. Please try again!
+                </p>
+            ) : null}
+          </span>
+      </form>
+
+      <section id="comment-list">
         <ul value={comments.length}>
-            {comments.map((element) => {
-                return <CommentCard key={element.comment_id} comment={element}/>
-            })}
+          {comments.map((element) => {
+            return <CommentCard key={element.comment_id} comment={element} />;
+          })}
         </ul>
-    </section>
-)
-
-
-
-}
+      </section>
+    </div>
+  );
+};
 
 export default CommentList;
