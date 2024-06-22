d0cc85e8a32a748350e017011354b8b847ef4467
Oliver1334 <oliver.taylor95@outlook.com>
2024-06-22 17:37:59 +0100

    review card css formatting so reviews are displayed in proper grid with no clipping

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,16 +1,27 @@
+import React from 'react';
 import {Link} from 'react-router-dom';
 
-const ReviewCard = ({ review }) => {
+export const ReviewCard = ({ review }) => {
+    const previewReview = review.review_body.substring(0, 100) + "...";
+    const previewShortReview = review.review_body.substring(0, 50) + "...";
 
-    const { category, comment_count, created_at, designer, owner, review_img_url, title, votes, review_id} = review;
+    const createdAtDate = new Date(review.created_at);
+
+    const formattedDate = `${createdAtDate.getDate().toString().padStart(2, '0')}-${(createdAtDate.getMonth() + 1).toString().padStart(2, '0')}-${createdAtDate.getFullYear()}`;
+    
+    const { category, comment_count, designer, owner, review_img_url, title, votes, review_id} = review;
+
     return (
-        <li>
+        
             <article>
-                <img src={review_img_url} alt="" />
+                <img src={review_img_url} alt="{title}" className='article-card-img' />
                 <h3>Title: {title} </h3>
-                <h3>Designer: {designer}</h3>
+            {title.length < 48 ? <p>{previewReview}</p> : <p>{previewShortReview}</p>}
+
                 <h3>Review by: {owner}</h3>
+                <h3>Designer: {designer}</h3>
                 <h4>Category: {category}</h4>
+                <h4>Posted on {formattedDate}</h4>
                 <h4>Votes: {votes}</h4>
                 <h4>Comments: {comment_count}</h4>
                 <div>
@@ -19,9 +30,8 @@ const ReviewCard = ({ review }) => {
                 </Link>
                 </div>
             </article>
-        </li>
+        
     )
-}
+};
 
 
-export default ReviewCard;
