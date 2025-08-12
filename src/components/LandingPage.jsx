559002b7249789a1f25e59abe749959b9dfe544a
Oliver1334 <oliver.taylor95@outlook.com>
2025-08-12 23:10:56 +0100

    Footer styling update

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,7 +5,7 @@ const LandingPage = () => {
 
 
 return (
-    <div className="justify-center items-center flex min-h-150 bg-white dark:bg-brandDark"> 
+    <div className="justify-center items-center flex min-h-150 bg-brandLight dark:bg-brandDark"> 
 
         {/* <p className="text-black dark:text-brandText">----------LANDING PAGE----------</p> */}
         <NDGLogoDice className="w-32 h-32"/>
